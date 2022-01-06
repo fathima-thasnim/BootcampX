29664f621cfff21a4fd30feb4250f864df0a1a60
fathima-thasnim <thasnimfathima492@gmail.com>
2022-01-06 22:20:36 +0000

    parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,17 +6,25 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
+
+const cohortName = process.argv[2] || 'JUL02';
+
+const queryString = `
+SELECT DISTINCT teachers.name as teacher_name, cohorts.name as cohort_name
+FROM assistance_requests
+JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+WHERE cohorts.name = $1
+ORDER BY teachers.name;
+`;
+ 
+const values = [cohortName];
+
+pool.query(queryString, values)
 .then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+  res.rows.forEach(user => {
+    console.log(`${user.cohort_name}: ${user.teacher_name}`);
   })
-});
+})
+.catch(err => console.error('query error', err.stack));
